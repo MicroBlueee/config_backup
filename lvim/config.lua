3befdc34eaf853e39c3a61bbbc324208fc4176e0
isaacsue <805890851@qq.com>
2024-11-27 00:45:08 +0800

    neovide

--- lvim/config.lua
+++ lvim/config.lua
@@ -523,8 +523,8 @@ lvim.autocommands = {
 }
 
 if vim.g.neovide then
-  -- vim.o.guifont = "Victor Mono:h14"
-  vim.g.neovide_input_macos_option_key_is_meta = true
+  vim.o.guifont = "Victor Mono:h14"
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
   vim.g.neovide_remember_window_size = true
   vim.g.neovide_cursor_vfx_mode = "sonicboom"
 end
