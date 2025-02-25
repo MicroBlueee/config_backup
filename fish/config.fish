d45c42b78f23e8c7d5984f9449019c0a9ead64e9
isaacsue <805890851@qq.com>
2025-02-25 12:28:00 +0800

    fish conda

--- fish/config.fish
+++ fish/config.fish
@@ -121,12 +121,12 @@ end
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /usr/local/Caskroom/miniconda/base/bin/conda
-    eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" hook $argv | source
+    eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
 else
     if test -f "/usr/local/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
         . "/usr/local/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH /usr/local/Caskroom/miniconda/base/bin $PATH
+        set -x PATH "/usr/local/Caskroom/miniconda/base/bin" $PATH
     end
 end
 # <<< conda initialize <<<
