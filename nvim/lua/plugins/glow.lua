f64c40476daff0fa65d4e88c7b171391c3e0c094
isaacsue <805890851@qq.com>
2025-03-10 14:10:08 +0800

    nvim插件添加

---
+++ nvim/lua/plugins/glow.lua
@@ -0,0 +1,8 @@
+-- You must install glow globally
+-- https://github.com/charmbracelet/glow
+-- yay -S glow
+return {
+    "npxbr/glow.nvim",
+    ft = {"markdown"}
+    -- build = "yay -S glow"
+}
--- nvim/lua/plugins/markdownpreview.lua
+++ nvim/lua/plugins/markdownpreview.lua
@@ -1,9 +1,12 @@
-return{
+return {
     "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    cmd = {"MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop"},
     build = "cd app && yarn install",
     init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
+        vim.g.mkdp_filetypes = {"markdown"}
     end,
-    ft = { "markdown" },
+    ft = {"markdown"},
+    config = function()
+        vim.g.mkdp_auto_start = 1
+    end
 }
