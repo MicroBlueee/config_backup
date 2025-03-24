5795e0876f69312c0c274b794f8af90ba0804a81
isaacsue <805890851@qq.com>
2025-03-24 11:46:38 +0800

    brew bundle

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,8 +5,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    # nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    nix-homebrew.url = "git+https://github.com/zhaofengli/nix-homebrew?ref=refs/pull/71/merge";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    # nix-homebrew.url = "git+https://github.com/zhaofengli/nix-homebrew?ref=refs/pull/71/merge";
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew,...}:
