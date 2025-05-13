cf6317f32700d0811e9f8fd690baf66e526a2d85
isaacsue <805890851@qq.com>
2025-05-13 22:21:03 +0800

    修正所有字体问题

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -63,7 +63,7 @@ opt.autochdir = true
 if vim.g.neovide then
     -- Put anything you want to happen only in Neovide here
     -- vim.opt.guifont = { "Iosevka", "h14" }
-    vim.opt.guifont = "Berkeley Mono:h14"
+    vim.opt.guifont = "Maple Mono NF CN:h14"
     vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
     -- vim.g.neovide_fullscreen = true
     vim.g.neovide_remember_window_size = true
